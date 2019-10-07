d8c9a0d7bca718fcd84117091844335c7c2f8a28
tracey brimage <onelovenilynn@gmail.com>
2019-10-07 23:05:10 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,10 @@
+   @@count=0
+class song(name,artist,genre)
+   initialize(name,artist,genre)
+ end
+ :attr_accessor(name,artist,genre)
+ class ninety_nine_problems= song.new("99 Problems", "Jay-Z", "rap")
+
+
+class.count
+song.genres
