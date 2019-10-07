81982c3633c1296abc70f5cc95ae712e15d0b3a3
tracey brimage <onelovenilynn@gmail.com>
2019-10-07 23:02:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,8 @@
+   @@count=0
 class song(name,artist,genre)
    initialize(name,artist,genre)
  end
  :attr_accessor(name,artist,genre)
-
+class.count
 
  class ninety_nine_problems= song.new("99 Problems", "Jay-Z", "rap")
-   @count=0
